943e85f87acb3903801106e7ee0064cade0f3787
Ronalmoo <ydaniel0826@gmail.com>
2019-10-09 05:24:47 +0900

    Use GPU

--- train.py
+++ train.py
@@ -1,15 +1,19 @@
 import torch
 import torch.nn as nn
 import torch.optim as optim
-from data_loader import data_loader
+from dataloader.data_loader import data_loader
 from tqdm import tqdm
 import argparse
-from model.ops import MLP, CNN
+from models.mlp import MLP
+from models.conv import CNN
+from models.resnet import ResNet50
 
 
 def train(args, model, device, train_loader, optimizer, epoch):
     model.train()
     for i, (data, target) in tqdm(enumerate(train_loader, 0)):
+        data = data.to(device)
+        target = target.to(device)
         optimizer.zero_grad()
         output = model(data)
         criterion = nn.CrossEntropyLoss()
@@ -24,7 +28,7 @@ def train(args, model, device, train_loader, optimizer, epoch):
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument('--model', type=str, default='MLP')
+    parser.add_argument('--model', type=str, default='ResNet50')
     parser.add_argument('--batch-size', type=int, default=64, metavar='N',
                         help='input batch size for training (default: 64)')
     parser.add_argument('--test-batch-size', type=int, default=1000, metavar='N',
@@ -42,22 +46,31 @@ def main():
 
     parser.add_argument('--save_model', action='store_true', default=False,
                         help='For Saving the current Model')
+    parser.add_argument('--cuda', action='store_true',
+                        help='use CUDA')
     args = parser.parse_args()
-    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+    if torch.cuda.is_available():
+        if not args.cuda:
+            print("WARNING: You have a CUDA device, so you should probably run with --cuda")
+
+    device = torch.device("cuda" if args.cuda else "cpu")
     train_loader = data_loader()[0]
 
-    if args.model == 'CNN':
-        model = CNN()
-    if args.model == 'MLP':
-        model = MLP()
+    # if args.model == 'CNN':
+    #     model = CNN()
+    # if args.model == 'MLP':
+    #     model = MLP()
+    model = ResNet50()
+    model.to(device)
 
     optimizer = optim.SGD(model.parameters(), lr=args.lr, momentum=args.momentum)
 
-    for epoch in range(1, args.epochs + 1):
+    for epoch in tqdm(range(1, args.epochs + 1)):
         train(args, model, device, train_loader, optimizer, epoch)
 
     if (args.save_model):
-        torch.save(model.state_dict(), "CIFAR-10_MLP.pt")
+        torch.save(model.state_dict(), "CIFAR-10_ResNet50.pt")
 
 
 if __name__ == "__main__":
