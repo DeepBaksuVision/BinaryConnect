b3e85a5e6da972facfe3b0858831edec88cdfbab
Ronalmoo <ydaniel0826@gmail.com>
2019-10-11 23:18:44 +0900

    add test.py

--- test.py
+++ test.py
@@ -1,26 +1,36 @@
 import torch
 from models.resnet import ResNet50
 from dataloader.data_loader import data_loader
-
+import argparse
 
 if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--cuda', action='store_true',
+                        help='use CUDA')
+    args = parser.parse_args()
     classes = ('plane', 'car', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
+    device = torch.device("cuda" if args.cuda else "cpu")
 
     test_loader = data_loader()[1]
     correct = 0
     total = 0
     model = ResNet50()
+    model.to(device)
     model.load_state_dict(torch.load('CIFAR-10_ResNet50.pt'))
 
     model.eval()
     with torch.no_grad():
         for data in test_loader:
             images, labels = data
+            images = images.to(device)
+            labels = labels.to(device)
+            model = model.to(device)
             outputs = model(images)
             _, predicted = torch.max(outputs.data, 1)
             total += labels.size(0)
             correct += (predicted == labels).sum().item()
-    print('Accuracy of the network on the 10000 test images: %d %%' % (100 * correct / total))
+            train_acc = 100 * correct / total
+    print('Accuracy of the network on the 10000 test images: %d %%' % (train_acc))
 
     # Class correct
     class_correct = list(0. for i in range(10))
@@ -28,6 +38,9 @@ if __name__ == "__main__":
     with torch.no_grad():
         for data in test_loader:
             images, labels = data
+            images = images.to(device)
+            labels = labels.to(device)
+            model = model.to(device)
             outputs = model(images)
             _, predicted = torch.max(outputs, 1)
             c = (predicted == labels).squeeze()
@@ -39,6 +52,3 @@ if __name__ == "__main__":
     for i in range(10):
         print('Accuracy of %5s : %2d %%' % (
             classes[i], 100 * class_correct[i] / class_total[i]))
-
-
-
