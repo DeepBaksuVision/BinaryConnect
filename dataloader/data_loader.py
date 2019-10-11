f2d8c7415b9f2496d978380ffd3e95a547057dff
Ronalmoo <ydaniel0826@gmail.com>
2019-10-11 23:17:53 +0900

    minor fixed

--- dataloader/data_loader.py
+++ dataloader/data_loader.py
@@ -5,7 +5,9 @@ import torchvision.transforms as transforms
 
 def data_loader():
     transform = transforms.Compose(
-        [transforms.ToTensor(), transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
+        [transforms.ToTensor(), transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))],
+
+    )
     train_set = torchvision.datasets.CIFAR10(root='./data', train=True, download=True, transform=transform)
     train_loader = torch.utils.data.DataLoader(train_set, batch_size=4, shuffle=True, num_workers=2)
     test_set = torchvision.datasets.CIFAR10(root='./data', train=False, download=True, transform=transform)
@@ -14,3 +16,4 @@ def data_loader():
     classes = ('plane', 'car', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
 
     return train_loader, test_loader, classes
+
