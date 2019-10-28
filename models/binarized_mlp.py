a4944296a88f234f5617c015e9e5ee3c734dac5b
chromatices <56014940+chromatices@users.noreply.github.com>
2019-10-28 14:09:09 +0900

    [JH/wandb] wandb에 weights visualization (#44)
    * # First commit
    
    * CNN Model
    
    * add argparse
    
    * Re-construct hierarchy (#11)
    
    *  Repository hierarchy reconstructed
    
    *  Add binarylinear
    
    * wandb gradient visualization

--- models/binarized_mlp.py
+++ models/binarized_mlp.py
@@ -8,11 +8,7 @@ from torchsummary import summary
 from torch.nn import functional as F
 from torch.utils.data import DataLoader
 from torchvision.datasets import MNIST
-<<<<<<< HEAD
 from utils.binarized_linear import BinarizedLinear
-=======
-from utils.BinarizedLinear import BinarizedLinear
->>>>>>> develop
 
 
 class Binarized_MLP(pl.LightningModule):
@@ -25,7 +21,7 @@ class Binarized_MLP(pl.LightningModule):
         self.dropout = nn.Dropout(0.2)
         self.batch2 = nn.BatchNorm1d(512)
         self.fc3 = BinarizedLinear(512, 10, bias=False, mode=mode, device=device)
-
+        
     def forward(self, x):
         x = x.view(-1, 28 * 28)
         x = torch.sigmoid(self.fc1(x))
---
+++ utils/BinaryLinear.py
@@ -0,0 +1,52 @@
+import copy
+import torch
+import torch.nn.functional as F
+
+
+class BinaryLinear(torch.nn.Linear):
+
+    def __init__(self, in_features, out_features, bias=True, mode="Stocastic"):
+        super().__init__(in_features, out_features, bias)
+        self.mode = mode
+        self.bin_weight = self.weight_binarization(self.weight, self.mode)
+
+    def forward(self, input: torch.Tensor) -> torch.Tensor:
+        self.clipping_weight(self.weight)
+        self.bin_weight = self.weight_binarization(self.weight, self.mode)
+        return F.linear(input, self.bin_weight, self.bias)
+
+    def weight_binarization(self, weight: torch.tensor, mode:str):
+        with torch.set_grad_enabled(False):
+            if mode == "Stocastic":
+                bin_weight = self.stocastic(weight)
+            elif mode == "Deterministic":
+                bin_weight = self.deterministic(weight)
+            else:
+                raise RuntimeError("{} is does not exist or not supported".format(mode))
+        bin_weight.requires_grad = True
+        bin_weight.register_hook(self.cp_bin_grad_to_real_grad_hook)
+        return bin_weight
+
+    @staticmethod
+    def deterministic(weight: torch.tensor) -> torch.tensor:
+        return weight.sign()
+
+    @staticmethod
+    def stocastic(weight: torch.tensor) -> torch.tensor:
+        p = torch.sigmoid(weight)
+        uniform_matrix = torch.empty(p.shape).uniform_(0,1)
+        bin_weight = (p >= uniform_matrix).type(torch.float32)
+        bin_weight[bin_weight==0] = -1
+        return bin_weight
+
+    def cp_bin_grad_to_real_grad_hook(self, grad):
+        # it not using deepcopy
+        # you will be meet Error as
+        # `Can't detach views in-place. Use detach() instead`
+        self.weight.grad = copy.deepcopy(grad)
+
+    def clipping_weight(self, weight:torch.tensor) -> torch.tensor:
+        with torch.set_grad_enabled(False):
+            weight = torch.clamp(weight, -1, 1)
+        weight.requires_grad = True
+        return weight
